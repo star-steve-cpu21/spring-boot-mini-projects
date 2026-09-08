d1c3150b96bc92ad4b7fe700733400a258678182
Steve Htun Thiha <325661954+star-steve-cpu21@users.noreply.github.com>
2026-09-08 16:28:46 +0800

    setup(project1): Initial setup of library-lending-service
    - Dependencies installed: Spring Boot DevTools, Spring Web, PostgreSQL JDBC, Lombok
    - JDK 21; Apache Maven 3.9.14; Spring Boot 4.1.1

---
+++ library-lending-service/src/main/java/com/steve/librarylendingservice/LibraryLendingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.steve.librarylendingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryLendingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryLendingServiceApplication.class, args);
+	}
+
+}
---
+++ library-lending-service/src/test/java/com/steve/librarylendingservice/LibraryLendingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steve.librarylendingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryLendingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
